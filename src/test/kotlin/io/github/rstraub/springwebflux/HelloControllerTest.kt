70b3f70167e767bbfd7d88f42dfb3268d4f9c1ea
Roy Straub <roy.straub@codesquad.nl>
2019-01-08 12:54:35 +0100

    Cleaner tests with mockito extensions for junit 5

--- src/test/kotlin/io/github/rstraub/springwebflux/HelloControllerTest.kt
+++ src/test/kotlin/io/github/rstraub/springwebflux/HelloControllerTest.kt
@@ -2,24 +2,36 @@ package io.github.rstraub.springwebflux
 
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
 import org.mockito.Mockito
-import org.mockito.Mockito.mock
+import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.test.web.reactive.server.WebTestClient
 import reactor.core.publisher.Flux
 
+/**
+ * Getting this to work took some extra work. Mainly setting up the jUnit 5 with the mockito extension.
+ * Read more: https://www.baeldung.com/mockito-junit-5-extension
+ */
+@ExtendWith(MockitoExtension::class)
 class HelloControllerTest {
+    @Mock
+    private lateinit var helloService: HelloService
+
+    @InjectMocks
+    private lateinit var controller: HelloController
+
     private lateinit var client: WebTestClient
 
     @BeforeEach
     fun setup() {
-        val service: HelloService = mock(HelloService::class.java)
-        Mockito.`when`(service.getHello()).thenReturn(Flux.empty())
-        val controller = HelloController(service)
+        Mockito.`when`(helloService.getHello()).thenReturn(Flux.empty())
 
         client = WebTestClient.bindToController(controller)
-            .configureClient()
-            .baseUrl("/")
-            .build()
+                .configureClient()
+                .baseUrl("/")
+                .build()
     }
 
     @Test
@@ -28,6 +40,6 @@ class HelloControllerTest {
                 .get()
                 .exchange()
                 .expectStatus()
-                .isOk
+                .is5xxServerError
     }
 }
